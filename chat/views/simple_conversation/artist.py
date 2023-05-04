fa3f6223516b2bccefbe4e6dc84df99796d8e989
BustDot <Bust.dev@outlook.com>
2023-05-04 18:50:51 +0800

    revise simple conversation

--- chat/views/simple_conversation/artist.py
+++ chat/views/simple_conversation/artist.py
@@ -2,19 +2,18 @@ import json
 
 
 def artist(relic_id):
-    with open('../../static/data.json', 'r') as file:
-        data = json.load(file)
-        for museum in data:
-            for relic in museum:
-                if relic["id"] == relic_id:
-                    res = ""
-                    if relic["artist"] != "" or relic["artist"].lower() != "unknown":
-                        res = f"这个文物是由{relic['artist']}创作的"
-                    else:
-                        res = "抱歉，这个文物的作者目前为止还是个迷。"
+    with open('chat/data/data.json', 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+    for museum, relics in json_data.items():
+        for relic in relics:
+            if relic["id"] == relic_id:
+                res = ""
+                if relic["artist"] != "" and relic["artist"].lower() != "unknown":
+                    res = f"这个文物是由{relic['artist']}创作的"
+                else:
+                    res = "抱歉，这个文物的作者目前为止还是个迷。"
 
-                    return {
-                        "relic_id": relic_id,
-                        "data": res,
-                        "result": "success"
-                    }
+                return {
+                    "relic_id": relic_id,
+                    "data": res,
+                }
--- chat/views/simple_conversation/simple_conversation.py
+++ chat/views/simple_conversation/simple_conversation.py
@@ -1,28 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import json
-
-with open('chat/static/data1.json', 'r') as file:
-    json_data = json.load(file)
-
-
-def artist(relic_id):
-    for museum in json_data:
-        for relic in museum:
-            if relic["id"] == relic_id:
-                res = ""
-                if relic["artist"] != "" or relic["artist"].lower() != "unknown":
-                    res = f"这个文物是由{relic['artist']}创作的"
-                else:
-                    res = "抱歉，这个文物的作者目前为止还是个迷。"
-
-                return {
-                    "relic_id": relic_id,
-                    "data": res,
-                    "result": "success"
-                }
-
+from chat.views.simple_conversation.artist import artist
 
 question_types = {
     'artist': artist,
@@ -31,16 +10,14 @@ question_types = {
 
 @api_view(['POST'])
 def simple_conversation(request):
-    data = {}
-    # try:
     relic_id = request.data.get('relic_id')
     question_type = request.data.get('question_type')
     if question_type in question_types:
-        func = question_types["question_type"]
+        func = question_types[question_type]
         print(func)
         data = func(relic_id)
-    response = {'data': data, 'result': 'success'}
-    return Response(data=response)
-    # except:
-    #     response = {'result': 'error'}
-    #     return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+        response = {'data': data, 'result': 'success'}
+        return Response(data=response)
+    else:
+        response = {'result': 'error', "message": 'invalid question_type.'}
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
